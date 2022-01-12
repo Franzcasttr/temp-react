eb329c930dd5228b5dcdbf536c34299b4633ab24
Franz <franiscatro.216.fc@gmail.com>
2022-01-12 22:41:56 +0800

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+        perspiciatis earum ea explicabo amet provident odit voluptatum obcaecati
+        reprehenderit ad, nihil ipsa recusandae nulla suscipit repellendus illo
+        omnis veritatis quia corrupti totam. Molestias laudantium fugiat ab
+        repudiandae distinctio veritatis possimus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
